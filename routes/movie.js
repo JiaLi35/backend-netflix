d3e97fd70105a0968c447bdbad299524c3a177fd
Jia Li <chin.j3li@gmail.com>
2025-08-12 11:06:57 +0800

    add, update and delete movies and tv shows

--- routes/movie.js
+++ routes/movie.js
@@ -56,4 +56,94 @@ router.get("/:id", async (req, res) => {
   res.send(movie);
 });
 
+/*
+  POST /movies - add new movie
+  this POST route need to accept the following parameters
+  - title
+  - director
+  - release_year
+  - genre
+  - rating
+*/
+router.post("/", async (req, res) => {
+  try {
+    const title = req.body.title;
+    const director = req.body.director;
+    const release_year = req.body.release_year;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+
+    // check error - make sure all the fields are not empty
+    if (!title || !director || !release_year || !genre || !rating) {
+      return res.status(400).send({
+        message: "All the fields are required.",
+      });
+    }
+
+    // create new movie
+    const newMovie = new Movie({
+      title: title,
+      director: director,
+      release_year: release_year,
+      genre: genre,
+      rating: rating,
+    });
+    // save the new movie into mongodb
+    await newMovie.save(); // clicking the "save" button
+    res.send(newMovie);
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
+// PUT /movies/68941d9dd6c0a0f6f210a9ae - update movie
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id; // id of the movie from url
+    const title = req.body.title;
+    const director = req.body.director;
+    const release_year = req.body.release_year;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+
+    // check error - make sure all the fields are not empty
+    if (!title || !director || !release_year || !genre || !rating) {
+      return res.status(400).send({
+        message: "All the fields are required.",
+      });
+    }
+
+    const updatedMovie = await Movie.findByIdAndUpdate(
+      id,
+      {
+        title: title,
+        director: director,
+        release_year: release_year,
+        genre: genre,
+        rating: rating,
+      },
+      {
+        new: true, // return the updated data
+      }
+    );
+
+    res.status(200).send(updatedMovie);
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
+// DELETE /movies/68941d9dd6c0a0f6f210a9ae - delete movie
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Movie.findByIdAndDelete(id);
+    res
+      .status(200)
+      .send({ message: `Movie with the ID of ${id} has been deleted.` });
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
 module.exports = router;
--- routes/tvshow.js
+++ routes/tvshow.js
@@ -36,4 +36,95 @@ router.get("/:id", async (req, res) => {
   res.send(tvshow);
 });
 
+// POST - add new movie
+router.post("/", async (req, res) => {
+  try {
+    const title = req.body.title;
+    const creator = req.body.creator;
+    const premiere_year = req.body.premiere_year;
+    const end_year = req.body.end_year; // optional
+    const seasons = req.body.seasons;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+
+    // check if fields are empty
+    if (!title || !creator || !premiere_year || !seasons || !genre || !rating) {
+      return res.status(400).send({
+        message: "All the fields are required.",
+      });
+    }
+
+    // create a new tvshow
+    const newTVShow = new TVShow({
+      title: title,
+      creator: creator,
+      premiere_year: premiere_year,
+      end_year: end_year,
+      seasons: seasons,
+      genre: genre,
+      rating: rating,
+    });
+
+    // save the new tvshow into mongodb
+    await newTVShow.save();
+    res.send(newTVShow);
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
+// PUT - update tvshow
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const title = req.body.title;
+    const creator = req.body.creator;
+    const premiere_year = req.body.premiere_year;
+    const end_year = req.body.end_year; // optional
+    const seasons = req.body.seasons;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+
+    // check if fields are empty
+    if (!title || !creator || !premiere_year || !seasons || !genre || !rating) {
+      return res.status(400).send({
+        message: "All the fields are required.",
+      });
+    }
+
+    const updatedTVShow = await TVShow.findByIdAndUpdate(
+      id,
+      {
+        title: title,
+        creator: creator,
+        premiere_year: premiere_year,
+        end_year: end_year,
+        seasons: seasons,
+        genre: genre,
+        rating: rating,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).send(updatedTVShow);
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
+// DELETE - delete tvshow
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await TVShow.findByIdAndDelete(id);
+    res
+      .status(200)
+      .send({ message: `TV show with the ID of ${id} has been deleted.` });
+  } catch (error) {
+    res.status(400).send({ message: "Unknown error" });
+  }
+});
+
 module.exports = router;
